416ed49687dc69b6dc7541decbe875bae48d30db
00DanielHarris <88143224+ZekDH@users.noreply.github.com>
2022-10-07 12:33:13 +1030

    Update projectPage.js

--- src/Components/projectPage.js
+++ src/Components/projectPage.js
@@ -25,45 +25,28 @@ export default class ProjectPage extends React.Component {
       <>
       <Box p={3} className="center">
         <Flex p={3} mx={-2} flexWrap="wrap" className="titleBox center">
+  
+          {/* Project box component with props parsed for display */}
           <ProjectBox
             projName="Blockle"
             projSkills="Javascript React Css Html"
             projDesc="A Wordle/Tetris inspired daily browser game that I had built to learn react and display web development skills outside of university."
             img={blockle}
-            gitLink=""
+            gitLink="https://github.com/ZekDH/BlockleWebsite"
             siteLink="http://blockle.net/"
             p={1} mainCol={this.props.mainCol} secCol={this.props.secCol} width={[ 1 , 1/2 ]}
           />
-
+          
+          {/* Project box component with props parsed for display */}
           <ProjectBox
             projName="Shader Engine"
             projSkills="C++ OpenGL ImGui"
             projDesc="An OpenGL shader engine I had developed then modified to meet my needs of editing shader uniforms in realtime that additionally uses ImGUI for the UI interface, I had learnt the process of rendering objects to a custom engine with this software."
             img={openGL}
-            gitLink=""
+            gitLink="https://github.com/ZekDH/OpenGLShaderEngine"
             siteLink=""
             p={1} mainCol={this.props.mainCol} secCol={this.props.secCol} width={[ 1 , 1/2 ]}
           />
-
-          {/* <ProjectBox
-            projName="Motion Tracking"
-            projSkills="Python OpenCV MediaPipe"
-            projDesc="Python code using mediapipe and openCV libraries to track pose and hand movements from users web camera."
-            img="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXIlMjBjb2RlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            gitLink=""
-            siteLink=""
-            p={1} mainCol={this.props.mainCol} secCol={this.props.secCol} width={[ 1 , 1/2 ]}
-          />
-
-          <ProjectBox
-            projName="Portfolio Site"
-            projSkills="React Javascript HTML CSS"
-            projDesc="Python code using mediapipe and openCV libraries to track pose and hand movements from users web camera."
-            img="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXIlMjBjb2RlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            gitLink=""
-            siteLink=""
-            p={1} mainCol={this.props.mainCol} secCol={this.props.secCol} width={[ 1 , 1/2 ]}
-          /> */}
 
         </Flex>
       </Box>
