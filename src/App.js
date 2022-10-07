9b740d037e2ad4673f4a2a8a87c4478564bc70a1
00DanielHarris <88143224+ZekDH@users.noreply.github.com>
2022-10-07 12:17:09 +1030

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import backgroundImage from './Img/background.png'
 
 import { FaBuffer, FaGraduationCap, FaHome } from "react-icons/fa";
 
-//Imported Packages
+//Imported Packages for responsive sizing
 import { Box, Flex, Text } from 'rebass'
 
 export default class App extends React.Component {
@@ -27,7 +27,8 @@ export default class App extends React.Component {
     this.changeGreen = this.changeGreen.bind(this);
   }
 
-
+  //Change to first tab by setting states
+  //Red and aqua based off colour spectrum
   changeRed(){
     this.setState({
       red: true,
@@ -38,7 +39,9 @@ export default class App extends React.Component {
     });
     return;
   }
-
+  
+  //Change to second tab by setting states
+  //Yellow and blue based off colour spectrum
   changeBlue(){
     this.setState({
       red: false,
@@ -50,6 +53,8 @@ export default class App extends React.Component {
     return;
   }
 
+  //Change to third tab by setting states
+  //Green and magenta based off colour spectrum
   changeGreen(){
     this.setState({
       red: false,
@@ -60,7 +65,8 @@ export default class App extends React.Component {
     });
     return;
   }
-
+  
+  //Startup function
   componentDidMount(){
     this.changeBlue();
   }
@@ -68,20 +74,22 @@ export default class App extends React.Component {
   render(){
     return(
       <>
+        {/*Header bar which lets you change between sections of the webpage*/}
+        {/*providing multiple width values allows for responsive resizing based off window size*/}
         <Box bg="#ededed" className="center headerbox headerPosition">
           <Flex px={4} py={4} className="center  titleBox" >
+  
             <Box onClick={this.changeBlue} className="titleText" width={[1/3, 2/8]}>
-
               <Text color={this.state.highlightColour} className="HeaderOne titleText" fontSize={[ 2, 3, 4 ]} fontWeight='bold'>
                 {this.state.blue && <FaHome color={this.state.highlightColourTwo} size={40} /> }
                 {!this.state.blue && <>Daniel Harris</>}
               </Text>
             </Box>
+                 
             <Box width={[0/3 , 2/8]}>
             </Box>
 
             <Box onClick={this.changeRed} width={[1/3 ,2/8]}>
-
               <Text color={this.state.highlightColour} className="HeaderOne titleText" fontSize={[ 2, 3, 4 ]} fontWeight='bold'>
                 {this.state.red && <FaBuffer color={this.state.highlightColourTwo} size={40} /> }
                 {!this.state.red && <>Projects</>}
@@ -89,16 +97,17 @@ export default class App extends React.Component {
             </Box>
 
             <Box onClick={this.changeGreen} width={[1/3, 2/8]}>
-
               <Text color={this.state.highlightColour} className="HeaderOne titleText" fontSize={[ 2, 3, 4 ]} fontWeight='bold'>
                 {this.state.green && <FaGraduationCap color={this.state.highlightColourTwo} size={40} /> }
                 {!this.state.green && <>University</>}
               </Text>
             </Box>
+                 
           </Flex>
         </Box>
 
-
+        {/*Complete pages converted into components to allow for neater code*/}
+        {/*States are used before compnent element to disable components based off state bool*/}
         {this.state.red && <><Box py={5}></Box> <ProjectPage className="body" mainCol={this.state.highlightColour} secCol={this.state.highlightColourTwo} /> </>}
         {this.state.blue && <><Box py={5}></Box> <HomePage className="body" img={backgroundImage} mainCol={this.state.highlightColour} secCol={this.state.highlightColourTwo} /> </>}
         {this.state.green && <><Box py={6}></Box> <UniPage className="body" mainCol={this.state.highlightColour} secCol={this.state.highlightColourTwo} /> </>}
